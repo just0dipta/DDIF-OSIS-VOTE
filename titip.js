b88385cfcf617fc867a851d39734122013d6d913
just0dipta <diptamahijav@gmail.com>
2025-08-30 16:33:57 +0700

    Add files via upload

---
+++ titip.js
@@ -0,0 +1,9 @@
+        const firebaseConfig = {
+          apiKey: "AIzaSyAOKl1IV8cGfHhpSPXidFX2x060PYkLafA",
+          authDomain: "hartig-project.firebaseapp.com",
+          databaseURL: "https://hartig-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "hartig-project",
+          storageBucket: "hartig-project.firebasestorage.app",
+          messagingSenderId: "584839002596",
+          appId: "1:584839002596:web:5d99bb50508daec8db6ff1"
+        };
